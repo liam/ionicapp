f8d62c6bcb080544c26e8a1e7787f9c8f4addfa0
liam missin <liam.missin@comparethemarket.com@>
2017-07-27 19:14:56 +0100

    add feed list onto first page

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { NavController, NavParams } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-hello-ionic',
@@ -9,7 +12,19 @@ export class HelloIonicPage {
   deadline: Date;
   counter: object;
 
-  constructor() {
+  newItems: any;
+  items: any;
+  BASE_URL: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
+    this.BASE_URL = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Fpolitics%2Fseries%2Feu-referendum-morning-briefing%2Frss";
+    this.newItems = [];
+    this.items = [];
+    //http.get(this.BASE_URL).map(res => res.json()).subscribe(data => {
+    //  this.items = data.items;
+    //});
+    this.getFeed(http, this.BASE_URL).then( newItems => this.updateItems(newItems));
+
     this.deadline = new Date('2019-03-29');
     this.counter = {
       days:12,
@@ -21,6 +36,25 @@ export class HelloIonicPage {
     setInterval(() => this.counter = getTimeRemaining(this.deadline), 1000);
 
     //setInterval(getTimeRemaining(this.deadline),1000);
+  }
+
+
+  getFeed(http, url){
+    return new Promise(function(resolve){ http.get(url, {}, {}).map(res => res.json()).subscribe(
+      data => { resolve(data.items); }
+    )});
+  };
+
+  updateItems(newItems) {
+    this.items = newItems;
+  }
+
+  loadMore() {
+    console.log('loading more...');
+  }
+
+  doRefresh() {
+    console.log('refreshing...');
   }
 }
   function getTimeRemaining(deadline: Date) {
@@ -38,5 +72,4 @@ export class HelloIonicPage {
       'minutes': minutes,
       'seconds': seconds
     };
-
 }
